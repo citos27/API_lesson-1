db87ad4d74250c96da250f9b00f0692293ed0f6f
citos27 <bulanov200158@gmail.com>
2026-02-09 11:33:38 +0600

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import requests
 
 
-def main():
-    cities = ["london", "svo", "Череповец"]
-    payload = {"nTqM": "", "lang": "ru"}
+cities = ["london", "svo", "Череповец"]
+
+
+def get_url():
+    urls = []
     for city in cities:
-        url_template = "http://wttr.in/{}"
-        url = url_template.format(city)
-        response = requests.get(url, params=payload)
-        response.raise_for_status()
-        print(response.text)
+        urls.append(f"http://wttr.in/{city}")
+    return urls
+
+
+def main(url):
+    payload = {"nTqM": "", "lang": "ru"}
+    response = requests.get(url, params=payload)
+    response.raise_for_status()
+    print(response.text)
 
 
 if __name__ == "__main__":
-    main()
+    urls = get_url()
+    for url in urls:
+        main(url)
+
